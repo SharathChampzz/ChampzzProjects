b9602d1d492d26cfd39643846e5a756c0defbf95
Sharath Kumar HK <sharathkumar.mskj.1999@gmail.com>
2024-06-16 14:00:44 +0530

    add temp blogs html changes

--- django/djangos/backend/blogs_api/models.py
+++ django/djangos/backend/blogs_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
