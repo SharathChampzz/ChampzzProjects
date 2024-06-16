c5a2b121813c1206199289e5887177860cf44c0e
Sharath Kumar HK <sharathkumar.mskj.1999@gmail.com>
2024-06-16 12:23:37 +0530

    Add backend API to perform CRUDs on blog

--- django/djangos/backend/blogs_api/urls.py
+++ django/djangos/backend/blogs_api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blogs, name='api-blogs'),
+    path('', views.blogs, name='blogs-blogs'),
+    path('<int:pk>', views.blog, name='blogs-blog'),
 ]
--- django/djangos/backend/blogs_api/views.py
+++ django/djangos/backend/blogs_api/views.py
@@ -1,9 +1,45 @@
 from rest_framework.decorators import api_view
-from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework import status
+from .models import Blog
+from .serializers import BlogSerializer
 
-
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def blogs(request: Request) -> Response:
-    return Response(data={'errorCode': 0, 'errorMessage': 'Successfull!'}, status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(blogs, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def blog(request: Request, pk: int) -> Response:
+    try:
+        blog = Blog.objects.get(pk=pk)
+    except Blog.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = BlogSerializer(blog)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT' or request.method == 'PATCH':
+        serializer = BlogSerializer(blog, data=request.data, partial=(request.method == 'PATCH'))
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        try:
+            blog.delete()
+            return Response({'message': 'blog deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response({'errorMessage': 'Failed to delete'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
